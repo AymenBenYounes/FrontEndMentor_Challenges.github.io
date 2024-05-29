2494f8520897745cdd39048e18b67b94a9871b9c
Aymen-benyounes008 <141510034+Aymen-benyounes008@users.noreply.github.com>
2024-05-30 00:35:24 +0200

    4th JS Challenge

---
+++ Javascript_Challenges/article-preview-component-master/script.js
@@ -0,0 +1,31 @@
+/* Mobile Animation */
+document.getElementById("open_share").addEventListener("click",function(){
+    var parent=document.getElementById("open_share").parentNode;
+    var grand_parent=parent.parentNode.parentNode;
+    var shown=grand_parent.lastElementChild;
+    var device_width=window.innerWidth;
+    if(parent.style.display!=="none" && device_width<1000){
+        parent.style.display="none";
+        shown.style.display="block";
+    }
+    else if(device_width>=1000){
+        if(shown.style.display==="none"){
+            document.getElementById("close_share").style.display="none";
+            shown.style.display="block";
+        }
+        else{
+            shown.style.display='';
+            shown.style.display='none';
+        }
+    }
+})
+document.getElementById("close_share").addEventListener("click",function(){
+    var parent=document.getElementById("close_share").parentNode.parentNode;
+    var shown=parent.parentNode.children[0].children[2];
+    if(parent.style.display!=="none"){
+        parent.style.display='';
+        parent.style.display="none";
+        shown.style.display='';
+        shown.style.display="flex";
+    }
+})
